8704560d641603bc6d823190fba8af19be9cbb01
ThankZ <natsuphat_th@cmu.ac.th>
2022-08-04 14:11:19 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  console.log(resp.data.result);
+  const name = [];
+  const address = [];
+  for (const key in resp.data.result) {
+    name.push(resp.data.name[key]);
+    address.push(key);
+  }
 }
+
+const name = document.getElementById("p-name");
+const hideuser = document.getElementById("div_user_card");
+const loadingcard = document.getElementById("div_loading_card");
